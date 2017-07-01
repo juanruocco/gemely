e2a309ce9a2bc2bdbf1ade202a232bfb76d844a9
Juan Montenegro <juanpabloruocco@gmail.com>
2017-06-30 23:12:27 -0500

    Improve UI: login, signup, header

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 
+gem 'jquery-rails'
+
+
+
 gem 'bootstrap-sass', '3.3.7'
 
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,14 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
   helper_method :current_user
+  helper_method :logged_in?
 
   def authorize
     redirect_to '/login' unless current_user
