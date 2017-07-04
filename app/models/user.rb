f09e73ccc9db8a663e5b67838ddc3c3dfafcf3a4
Juan Montenegro <juanpabloruocco@gmail.com>
2017-07-03 20:23:46 -0500

    Add validation to User and tests for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,21 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
 
+  before_save { self.email = email.downcase }
+
   field :name, type: String
   field :email, type: String
-  field :password_digest, type: String
 
+  field :password_digest, type: String
 
   has_many :likes
   has_secure_password
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+
 end
